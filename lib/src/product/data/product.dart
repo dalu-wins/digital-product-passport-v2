5d85a0238f602833417a2dd26e3c5840df247168
Dalu Wins <wins.daniil@proton.me>
2025-04-30 23:27:59 +0200

    added images preview to product screen

--- lib/src/product/data/product.dart
+++ lib/src/product/data/product.dart
@@ -11,11 +11,58 @@ class Product {
     required this.submodels,
   });
   Widget display(BuildContext context) {
+    var imagesSubmodel = getSubmodelByIdShort('Images');
+    List<Widget> displayedElements = [];
+    if (imagesSubmodel != null) {
+      displayedElements = imagesSubmodel.submodelElements
+          .map(
+            (element) => element.display(context),
+          )
+          .toList();
+    }
+
+    ConstrainedBox images = ConstrainedBox(
+      constraints: const BoxConstraints(maxHeight: 200),
+      child: CarouselView(
+        itemSnapping: true,
+        itemExtent: 330,
+        shrinkExtent: 200,
+        children: displayedElements,
+        onTap: (int index) {
+          showDialog(
+            context: context,
+            builder: (_) => Dialog(
+              backgroundColor: Colors.black,
+              insetPadding: EdgeInsets.zero,
+              child: InteractiveViewer(
+                child: Center(
+                  child: displayedElements[index],
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+
     submodels.sort();
     var submodelWidgets =
         submodels.map((submodel) => submodel.display(context)).toList();
+
     return Column(
-      children: submodelWidgets,
+      children: [
+        images,
+        ...submodelWidgets,
+      ],
     );
+  }
+
+  Submodel? getSubmodelByIdShort(String targetIdShort) {
+    try {
+      return submodels
+          .firstWhere((submodel) => submodel.idShort == targetIdShort);
+    } catch (e) {
+      return null; // if not found
+    }
   }
 }
