744566c3431446d56b6685ccb0caad35e079bc8d
Dalu Wins <wins.daniil@proton.me>
2025-05-01 00:20:47 +0200

    added hardcoded product screen, moved all submodels into separate tab

--- lib/src/product/data/product.dart
+++ lib/src/product/data/product.dart
@@ -1,4 +1,5 @@
 import 'package:digital_product_passport/src/faaast_flutter/models/submodel.dart';
+import 'package:digital_product_passport/src/product/presentation/aas_element_screen.dart';
 import 'package:flutter/material.dart';
 
 class Product {
@@ -11,6 +12,18 @@ class Product {
     required this.submodels,
   });
   Widget display(BuildContext context) {
+    return Column(
+      spacing: 8,
+      children: [
+        buildImages(context),
+        buildGeneralSection(context),
+        buildDetailSection(context),
+        buildAllSubmodelsCard(context),
+      ],
+    );
+  }
+
+  Widget buildImages(BuildContext context) {
     var imagesSubmodel = getSubmodelByIdShort('Images');
     List<Widget> displayedElements = [];
     if (imagesSubmodel != null) {
@@ -21,7 +34,7 @@ class Product {
           .toList();
     }
 
-    ConstrainedBox images = ConstrainedBox(
+    return ConstrainedBox(
       constraints: const BoxConstraints(maxHeight: 200),
       child: CarouselView(
         itemSnapping: true,
@@ -44,16 +57,226 @@ class Product {
         },
       ),
     );
+  }
 
-    submodels.sort();
-    var submodelWidgets =
-        submodels.map((submodel) => submodel.display(context)).toList();
-
+  // TODO Don't hardcode it
+  Widget buildGeneralSection(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        images,
-        ...submodelWidgets,
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(
+            "General",
+            style: Theme.of(context).textTheme.bodyLarge,
+          ),
+        ),
+        Card.outlined(
+          child: Padding(
+            padding:
+                const EdgeInsets.symmetric(horizontal: 16.0, vertical: 12.0),
+            child: Column(
+              spacing: 8,
+              children: [
+                Row(
+                  children: [
+                    Expanded(
+                      child: Text(
+                        "Manufacturer",
+                        softWrap: true,
+                        style: Theme.of(context).textTheme.bodyMedium,
+                      ),
+                    ),
+                    Expanded(
+                      child: Text(
+                        "Manufacturer Name",
+                        softWrap: true,
+                        style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                            color: Theme.of(context).colorScheme.primary),
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Text(
+                        "Model",
+                        softWrap: true,
+                        style: Theme.of(context).textTheme.bodyMedium,
+                      ),
+                    ),
+                    Expanded(
+                      child: Text(
+                        "Model Name",
+                        softWrap: true,
+                        style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                            color: Theme.of(context).colorScheme.primary),
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Text(
+                        "Type",
+                        softWrap: true,
+                        style: Theme.of(context).textTheme.bodyMedium,
+                      ),
+                    ),
+                    Expanded(
+                      child: Text(
+                        "Product Type",
+                        softWrap: true,
+                        style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                            color: Theme.of(context).colorScheme.primary),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
       ],
+    );
+  }
+
+  // TODO Don't hardcode it!
+  Widget buildDetailSection(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(
+            "Details",
+            style: Theme.of(context).textTheme.bodyLarge,
+          ),
+        ),
+        Card.outlined(
+          child: Padding(
+            padding:
+                const EdgeInsets.symmetric(horizontal: 16.0, vertical: 12.0),
+            child: Column(
+              spacing: 8,
+              children: [
+                Row(
+                  children: [
+                    Expanded(
+                      child: Text(
+                        "Screen Size",
+                        softWrap: true,
+                        style: Theme.of(context).textTheme.bodyMedium,
+                      ),
+                    ),
+                    Expanded(
+                      child: Text(
+                        '6.3"',
+                        softWrap: true,
+                        style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                            color: Theme.of(context).colorScheme.primary),
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Text(
+                        "Battery Capacity",
+                        softWrap: true,
+                        style: Theme.of(context).textTheme.bodyMedium,
+                      ),
+                    ),
+                    Expanded(
+                      child: Text(
+                        "4000 mAh",
+                        softWrap: true,
+                        style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                            color: Theme.of(context).colorScheme.primary),
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Text(
+                        "Battery Technology",
+                        softWrap: true,
+                        style: Theme.of(context).textTheme.bodyMedium,
+                      ),
+                    ),
+                    Expanded(
+                      child: Text(
+                        "LiPo",
+                        softWrap: true,
+                        style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                            color: Theme.of(context).colorScheme.primary),
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Text(
+                        "Operating System",
+                        softWrap: true,
+                        style: Theme.of(context).textTheme.bodyMedium,
+                      ),
+                    ),
+                    Expanded(
+                      child: Text(
+                        "iOS 18.0.1",
+                        softWrap: true,
+                        style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                            color: Theme.of(context).colorScheme.primary),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget buildAllSubmodelsCard(BuildContext context) {
+    submodels.sort();
+    return Card.outlined(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+        child: Row(
+          children: [
+            Expanded(
+              child: Text(
+                "All Submodels",
+                softWrap: true,
+                style: Theme.of(context).textTheme.bodyLarge,
+              ),
+            ),
+            IconButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => AASElementListScreen(
+                      elementName: "All Submodels",
+                      elements: submodels,
+                    ),
+                  ),
+                );
+              },
+              icon: Icon(Icons.chevron_right),
+            ),
+          ],
+        ),
+      ),
     );
   }
 
